c5085d798fa598d7f9f1ee7c4ddbc2e42920640f
gbvsCloud <gabriielblock@gmail.com>
2024-01-28 01:39:52 -0300

    Basic death ground system

--- Assets/Scripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerDeath.cs
@@ -8,7 +8,7 @@ public class PlayerDeath : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("Enemy"))
+        if (other.gameObject.CompareTag("Enemy") || other.gameObject.CompareTag("DeathGround"))
         {
            ReiniciarPartida();
         }
